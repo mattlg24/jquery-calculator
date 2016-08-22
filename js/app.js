06c1ae282ed02976ed139356193a8accffa5ba33
Matt Gardner <matthewlgardner@gmail.com>
2016-08-22 16:36:25 -0600

    changed operator vars to indexOf, added if statement to operator event listener

--- js/app.js
+++ js/app.js
@@ -2,36 +2,51 @@ console.log('I am connected');
 
 // number event handler
 $('.buttons').on('click', function(event) {
-let number1 = parseFloat($(event.target).text());
-$('#screen').append(number1)
+    let number = parseFloat($(event.target).text());
+    $('#screen').append(number)
 })
 
 // operator event handler
-$('.buttons').on('click', '.operator',function(event) {
-  let operator = ($(event.target).text());
-  $('#screen').append(operator)
-  console.log(operator);
+$('.buttons').on('click', '.operator', function(event) {
+    var operator = ($(event.target).text());
+    if (operator !== '=') {
+        $('#screen').append(operator)
+    } else {
+        console.log(operator)
+
+    }
 })
 
 // clear event listener
 $('.buttons').on('click', '#clear', function() {
-  location.reload()
+    location.reload()
 })
-
 
 $('#equals').on('click', function() {
-let equation = $('#screen').text()
+    console.log('hi');
+    let equation = $('#screen').text()
+    $('#screen').empty()
 
+    let multiplyOperator = equation.indexOf('x')
+    let divideOperator = equation.indexOf('÷')
+    let addOperator = equation.indexOf('+')
+    let subtractOperator = equation.indexOf('-')
+    let total = 0
+    let number1 = ''
+    let number2 = ''
 
-  let multiplyOperator = equation.includes('X')
-  let divideOperator = equation.includes('÷')
-  let addOperator = equation.includes('+')
-  let subtractOperator = equation.includes('-')
+    if (multiplyOperator > 0) {
+        number1 = parseInt(equation.substring(0, multiplyOperator))
+        number2 = parseInt(equation.substring(multiplyOperator + 1))
+        total = number1 * number2
+        $('#screen').append(total)
+    }
 
-  if (multiplyOperator === true) {
-  
-  }
+    if (divideOperator > 0) {
+        number1 = parseInt(equation.substring(0, multiplyOperator))
+        number2 = parseInt(equation.substring(multiplyOperator + 1))
+        total = number1 / number2
+        $('#screen').append(total)
+    }
 
-
-
-})
+});
